0febb58dcfdc6d0d86046736c6e5315f40d0d622
Anastasiia Savchuk <savchuknesty@gmail.com>
2020-08-28 21:54:13 +0300

    changes

--- src/main/java/wooden_houses/repository/HouseTechnologyRepository.java
+++
@@ -1,9 +0,0 @@
-package wooden_houses.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import wooden_houses.domain.HouseTechnology;
-
-@Repository
-public interface HouseTechnologyRepository extends JpaRepository<HouseTechnology, Integer> {
-}
