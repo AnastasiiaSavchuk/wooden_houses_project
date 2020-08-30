78f048cb362df8de857beb319e1b40a96a8bed62
Anastasiia Savchuk <savchuknesty@gmail.com>
2020-08-30 21:26:17 +0300

    changes

--- src/main/java/wooden_houses/domain/HouseBuilding.java
+++ src/main/java/wooden_houses/domain/HouseBuilding.java
@@ -22,11 +22,46 @@ public class HouseBuilding {
     private String name;
 
     @Lob
-    @Column(name = "description")
-    private String description;
+    @Column(name = "description_1")
+    private String description1;
 
-    public HouseBuilding(String name, String description) {
+    @Lob
+    @Column(name = "description_2")
+    private String description2;
+
+    @Lob
+    @Column(name = "description_3")
+    private String description3;
+
+    @Lob
+    @Column(name = "description_4")
+    private String description4;
+
+    @Lob
+    @Column(name = "description_5")
+    private String description5;
+
+    @Lob
+    @Column(name = "description_6")
+    private String description6;
+
+    @Lob
+    @Column(name = "description_7")
+    private String description7;
+
+    @Lob
+    @Column(name = "description_8")
+    private String description8;
+
+    public HouseBuilding(String name, String description1, String description2, String description3, String description4, String description5, String description6, String description7, String description8) {
         this.name = name;
-        this.description = description;
+        this.description1 = description1;
+        this.description2 = description2;
+        this.description3 = description3;
+        this.description4 = description4;
+        this.description5 = description5;
+        this.description6 = description6;
+        this.description7 = description7;
+        this.description8 = description8;
     }
 }
