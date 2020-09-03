d960a0a9f98bd6cb5168d60e4decf5b3910cbe42
Anastasiia Savchuk <savchuknesty@gmail.com>
2020-09-03 20:16:11 +0300

    changes

--- src/main/java/wooden_houses/service/impl/HouseImagesServiceImpl.java
+++ src/main/java/wooden_houses/service/impl/HouseImagesServiceImpl.java
@@ -18,8 +18,14 @@ public class HouseImagesServiceImpl implements HouseImagesService {
 
     @SneakyThrows
     @Override
-    public HouseImages save(MultipartFile image) {
-        return repository.save(new HouseImages(image.getBytes()));
+    public HouseImages save(MultipartFile image1, MultipartFile image2, MultipartFile image3, MultipartFile image4,
+                            MultipartFile image5, MultipartFile image6, MultipartFile image7, MultipartFile image8,
+                            MultipartFile image9, MultipartFile image10, MultipartFile image11, MultipartFile image12,
+                            MultipartFile groundFloor, MultipartFile firstFloor, MultipartFile basement) {
+        return repository.save(new HouseImages(image1.getBytes(), image2.getBytes(), image3.getBytes(), image4.getBytes(),
+                image5.getBytes(), image6.getBytes(), image7.getBytes(), image8.getBytes(), image9.getBytes(),
+                image10.getBytes(), image11.getBytes(), image12.getBytes(), groundFloor.getBytes(), firstFloor.getBytes(),
+                basement.getBytes()));
     }
 
     @Override
@@ -37,11 +43,11 @@ public class HouseImagesServiceImpl implements HouseImagesService {
     public HouseImages update(MultipartFile image1, MultipartFile image2, MultipartFile image3, MultipartFile image4,
                               MultipartFile image5, MultipartFile image6, MultipartFile image7, MultipartFile image8,
                               MultipartFile image9, MultipartFile image10, MultipartFile image11, MultipartFile image12,
-                              MultipartFile image13, MultipartFile image14, MultipartFile image15) {
+                              MultipartFile groundFloor, MultipartFile firstFloor, MultipartFile basement) {
         return repository.save(new HouseImages(image1.getBytes(), image2.getBytes(), image3.getBytes(), image4.getBytes(),
                 image5.getBytes(), image6.getBytes(), image7.getBytes(), image8.getBytes(), image9.getBytes(),
-                image10.getBytes(), image11.getBytes(), image12.getBytes(), image13.getBytes(), image14.getBytes(),
-                image15.getBytes()));
+                image10.getBytes(), image11.getBytes(), image12.getBytes(), groundFloor.getBytes(), firstFloor.getBytes(),
+                basement.getBytes()));
     }
 
     @Override
