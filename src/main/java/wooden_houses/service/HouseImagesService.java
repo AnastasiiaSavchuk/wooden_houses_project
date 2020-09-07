a71f31104a4e0aeed48b2cc320ac1469afc54970
Anastasiia Savchuk <savchuknesty@gmail.com>
2020-09-07 17:30:28 +0300

    changes

--- src/main/java/wooden_houses/service/HouseImagesService.java
+++ src/main/java/wooden_houses/service/HouseImagesService.java
@@ -1,25 +1,18 @@
 package wooden_houses.service;
 
-import org.springframework.web.multipart.MultipartFile;
 import wooden_houses.domain.HouseImages;
 
 import java.util.List;
 
 public interface HouseImagesService {
 
-    HouseImages save(MultipartFile image1, MultipartFile image2, MultipartFile image3, MultipartFile image4,
-                     MultipartFile image5, MultipartFile image6, MultipartFile image7, MultipartFile image8,
-                     MultipartFile image9, MultipartFile image10, MultipartFile image11, MultipartFile image12,
-                     MultipartFile groundFloor, MultipartFile firstFloor, MultipartFile basement);
+    HouseImages save(HouseImages images);
 
     HouseImages findOne(int id);
 
     List<HouseImages> findAll();
 
-    HouseImages update(int id, MultipartFile image1, MultipartFile image2, MultipartFile image3, MultipartFile image4,
-                       MultipartFile image5, MultipartFile image6, MultipartFile image7, MultipartFile image8,
-                       MultipartFile image9, MultipartFile image10, MultipartFile image11, MultipartFile image12,
-                       MultipartFile groundFloor, MultipartFile firstFloor, MultipartFile basement);
+    HouseImages update(HouseImages images);
 
     void delete(int id);
 
--- src/main/java/wooden_houses/service/impl/HouseImagesServiceImpl.java
+++ src/main/java/wooden_houses/service/impl/HouseImagesServiceImpl.java
@@ -3,7 +3,6 @@ package wooden_houses.service.impl;
 import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 import wooden_houses.domain.HouseImages;
 import wooden_houses.repository.HouseImagesRepository;
 import wooden_houses.service.HouseImagesService;
@@ -18,14 +17,8 @@ public class HouseImagesServiceImpl implements HouseImagesService {
 
     @SneakyThrows
     @Override
-    public HouseImages save(MultipartFile image1, MultipartFile image2, MultipartFile image3, MultipartFile image4,
-                            MultipartFile image5, MultipartFile image6, MultipartFile image7, MultipartFile image8,
-                            MultipartFile image9, MultipartFile image10, MultipartFile image11, MultipartFile image12,
-                            MultipartFile groundFloor, MultipartFile firstFloor, MultipartFile basement) {
-        return repository.save(new HouseImages(image1.getBytes(), image2.getBytes(), image3.getBytes(), image4.getBytes(),
-                image5.getBytes(), image6.getBytes(), image7.getBytes(), image8.getBytes(), image9.getBytes(),
-                image10.getBytes(), image11.getBytes(), image12.getBytes(), groundFloor.getBytes(), firstFloor.getBytes(),
-                basement.getBytes()));
+    public HouseImages save(HouseImages images) {
+        return repository.save(images);
     }
 
     @Override
@@ -40,14 +33,8 @@ public class HouseImagesServiceImpl implements HouseImagesService {
 
     @SneakyThrows
     @Override
-    public HouseImages update(int id, MultipartFile image1, MultipartFile image2, MultipartFile image3, MultipartFile image4,
-                              MultipartFile image5, MultipartFile image6, MultipartFile image7, MultipartFile image8,
-                              MultipartFile image9, MultipartFile image10, MultipartFile image11, MultipartFile image12,
-                              MultipartFile groundFloor, MultipartFile firstFloor, MultipartFile basement) {
-        return repository.save(new HouseImages(id, image1.getBytes(), image2.getBytes(), image3.getBytes(), image4.getBytes(),
-                image5.getBytes(), image6.getBytes(), image7.getBytes(), image8.getBytes(), image9.getBytes(),
-                image10.getBytes(), image11.getBytes(), image12.getBytes(), groundFloor.getBytes(), firstFloor.getBytes(),
-                basement.getBytes()));
+    public HouseImages update(HouseImages images) {
+        return repository.save(images);
     }
 
     @Override
